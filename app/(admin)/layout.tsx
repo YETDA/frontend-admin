b557d30d3a4e43585c6226aac47a99c769cb988a
smimdla0205 <sangmin0205@gmail.com>
2025-07-09 20:56:59 +0900

    feat: AdminLayout으로 레이아웃  초기 생성 #1

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,9 +1,6 @@
+import DashboardLayout from '@/components/layout/dashboardLayout';
 import type { ReactNode } from 'react';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
-  );
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return <DashboardLayout>{children}</DashboardLayout>;
 }
